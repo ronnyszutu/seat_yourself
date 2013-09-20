35460c0c12db7452699e59510b9832ba50b66fa4
Szuturon <ronnyszutu@gmail.com>
2013-09-19 21:20:51 -0400

    Write diner controller methods

--- app/controllers/diners_controller.rb
+++ app/controllers/diners_controller.rb
@@ -1,2 +1,48 @@
 class DinersController < ApplicationController
+	def new
+		@diner = Diner.new
+	end
+
+	def create
+		@diner = Diner.new diner_params	
+
+		if @diner.save
+			redirect_to diners_url
+		else
+			render :new
+		end
+	end
+
+	def show
+		@diner = Diner.find(params[:id])
+	end
+
+	def index
+		@diner = Diner.all
+	end
+
+	def edit
+		@diner = Diner.find(params[:id])
+	end
+
+	def update
+		@diner = Diner.find(params[:id])
+
+		if @diner.update_attributes diner_params
+			redirect_to diner_path(@diner)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@diner = Diner.find(params[:id])
+		@diner.destroy
+		redirect_to diner_path
+	end
+
+	private
+	def diner_params
+		params.require(:diner).permit(:name, :email, :password, :password_confirmation)
+	end
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,29 @@
 class RestaurantsController < ApplicationController
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def show
+		
+	end
+
+	def index
+		
+	end
+
+	def edit
+		
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
 end
