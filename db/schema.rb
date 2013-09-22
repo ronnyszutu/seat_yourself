0148d527dfedbb9cc769e772a782269ed3aa219f
Szuturon <ronnyszutu@gmail.com>
2013-09-22 01:42:13 -0400

    Add Restaurant_owner ID to Restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130920002108) do
+ActiveRecord::Schema.define(version: 20130922053241) do
 
   create_table "diners", force: true do |t|
     t.string   "name"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20130920002108) do
     t.string   "phonenumber"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "restaurant_owner_id"
   end
 
 end
